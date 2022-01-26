3d0f92c3747823b8c902d5a1b1f5b48267e5f243
shoIwasaki <>
2022-01-27 00:26:00 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState(Array());
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setUsers(data[0].name));
+    // .then((data) => console.log(data));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FRONT</h1>
+      {users}
     </div>
   );
 }
