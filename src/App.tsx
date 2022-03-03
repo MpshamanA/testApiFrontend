fc525e6447ddd62e1b3f3e9163436bb1037978b9
shoIwasaki <m.pshaman.a@gmail.com>
2022-03-04 00:15:43 +0900

    postロジック追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,58 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import axios from "axios";
 import "./App.css";
 
+type Users = {
+  name: string;
+  position: string;
+};
+
 function App() {
-  const [users, setUsers] = useState(Array());
+  const [users, setUsers] = useState<Users[]>();
+  const [inputName, setInputName] = useState<string>("");
+
+  const handleChage = (e: any): void => {
+    setInputName(e.target.value);
+  };
+
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setUsers(data[0].name));
-    // .then((data) => console.log(data));
-  });
+    let unmount = false;
+    if (!unmount) {
+      console.log("useEffect1が実行されました");
+      fetch("/api")
+        .then((res) => res.json())
+        .then((data) => setUsers(data));
+    }
+    return () => {
+      unmount = true;
+    };
+  }, []);
+
+  const testApi = () => {
+    axios
+      .post("/api", { name: inputName, position: "福岡" })
+      .then((res) => {
+        const test = res.data;
+        console.log(test);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className="App">
       <h1>FRONT</h1>
-      {users}
+      <input type="text" value={inputName} onChange={handleChage} />
+      <div>
+        {users &&
+          users.map((user: Users) => (
+            <div key={user.name}>
+              {user.name}：{user.position}
+            </div>
+          ))}
+        <button onClick={testApi}>API</button>
+      </div>
     </div>
   );
 }
